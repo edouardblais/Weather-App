d811e13f76756aa49def26a8ab8c7a4ee7956bc5
edouard <edouard.blais@hotmail.com>
2022-06-28 20:34:30 -0400

    fixed https

--- src/dailyWeather.js
+++ src/dailyWeather.js
@@ -22,10 +22,10 @@ const dailyWeather = (() => {
 
   async function getAPIData(cityname) {
     try {
-      const firstresponse = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=1&appid=8c958c34c399d529ae6bb1e94b934485`, { mode: 'cors' });
+      const firstresponse = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=1&appid=8c958c34c399d529ae6bb1e94b934485`, { mode: 'cors' });
       const databycityname = await firstresponse.json();
       const latlon = [databycityname[0].lat, databycityname[0].lon];
-      const secondresponse = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${latlon[0]}&lon=${latlon[1]}&appid=8c958c34c399d529ae6bb1e94b934485`);
+      const secondresponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latlon[0]}&lon=${latlon[1]}&appid=8c958c34c399d529ae6bb1e94b934485`);
       const databylatlon = await secondresponse.json();
       const APIdata = getSpecificData(databylatlon);
       return APIdata;
